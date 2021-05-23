8c7248233c078f14f003deef5deef3d1f2bc7d0f
pinooxd <pinooxd@gmail.com>
2021-05-23 16:52:16 -0700

    website list

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-to work in the backgroun in unix:
-  `sudo contrab -e`
-and add:
-  `@reboot python3 /path/to/python/script.py`
+working hours websites block
--- index.py
+++ index.py
@@ -1,49 +1,53 @@
 import time
 from datetime import datetime as dt
 
-# Host Files
-# Windows: c:\\windows\system32\drivers\etc
-# Mac & Linux: /etc/hosts
+# list of websites to block
+website_list = ["www.facebook.com", 
+                "facebook.com", 
+                "mail.google.com",
+                "www.youtube.com",
+                "www.twitter.com"]
+
+# define day working hours
+from_hour = 9
+to_hour = 17
 
 # make a copy of your hosts, as a backup
-hosts_path_windows = r"C:\Windows\System32\drivers\etc\hosts"
-hosts_path_unix = "/etc/hosts"
+host_path_windows = r"C:\Windows\System32\drivers\etc\hosts" # hosts file windows route 
+host_path_unix = "etc\hosts"                                 # hosts file unix route
+
 # host_dir = "hosts"
-host_dir =  hosts_path_unix 
+host_dir = host_path_windows
 
-redirect = "127.0.0.1"
+redirect = "127.0.0.1" # redirect site
 
-# list of websites to block
-website_list = [
-    "www.facebook.com",
-    "facebook.com",
-    "mail.google.com"
-]
 
-# Define working hours
-from_hour = 2
-to_hour = 16
 
-# Main Program
+# Main Loop
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, from_hour) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, to_hour):
+    if dt(dt.now().year,
+          dt.now().month,
+          dt.now().day, from_hour) < dt.now() < dt(dt.now().year,
+                                                   dt.now().month,
+                                                   dt.now().day, to_hour):
         print("Working hours...")
-        with open(host_dir, 'r+') as file:
+        with open(host_dir, 'r+') as file:   # open host file
             content = file.read()
-            for website in website_list:
+            for website in website_list:  # for list of websites
                 if website in content:
                     pass
                 else:
                     file.write(redirect + " " + website + "\n")
-    else:
+    # delete websites in fun hours
+    else:  
         with open(host_dir, 'r+') as file:
             content = file.readlines()
             file.seek(0)
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
-            file.truncate() 
-        print("Fun Hours")
-    time.sleep(1) #seconds
-
-
+            file.truncate()
+        print("Fun Hours", dt.now(), dt(dt.now().year,
+          dt.now().month,
+          dt.now().day, from_hour))
+    time.sleep(1)  #seconds
